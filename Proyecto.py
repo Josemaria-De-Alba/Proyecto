d0fb990280eef97726d4c7ce061b4a9fa9510dad
Josemaria-De-Alba <142268975+Josemaria-De-Alba@users.noreply.github.com>
2023-09-21 21:44:02 -0600

    Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -19,47 +19,63 @@ indicar si son suficientemente rapidos para tansportar los productos sin que
 se acumulen los materiales. Si son muchos materiales necesitas mejores conveyors
 """
 
-iron_ppm=int(input("How much iron ore do you produce per minute"))
-"""
-Este input recibe un numero que se usa para definir que tanto matieral produces y luego
-lo convierte en otro valor para decirte cuanto puedes construir
-"""
+print("Welcome to the Ficsit Efficient Factory Module")
 
-iron_sheets=iron_ppm//30
-iron_rods=iron_ppm//15
-screws=iron_rods//.66
-"""
-Las operaciones calculan cuantas lineas de produccion puedes hacer para cada material
-usando una division. Si necesitas 30 de hierro para una linea, divide el hierro que
-tienes entre 30 y te da el numero esacto de lineas de producion que puedes hacer.
-"""
+active_program=0
 
-if iron_sheets>=1:
-    print("You can produce",iron_sheets,"lines of iron sheets")
-    smelt_and_construct(iron_ppm,iron_sheets)
-    conveyors(iron_sheets)
+iron_ppm=0
+
+
+while active_program!=1:
+    active_program=int(input("What ore does your factory use? \n"
+                             "Press 1 To End Program \n"
+                             "Press 2 For Iron"))
+    if active_program==2:
+        iron_ppm=int(input("How much iron ore do you produce per minute in this module"))
+        """
+        Este input recibe un numero que se usa para definir que tanto matieral
+        produces y luego lo convierte en otro valor para decirte
+        cuanto puedes construir.
+        """
+
+        iron_sheets=iron_ppm//30
+        iron_rods=iron_ppm//15
+        screws=iron_rods//.66
+        
+        """
+        Las operaciones calculan cuantas lineas de produccion puedes hacer
+        para cada material usando una division. Si necesitas 30 de hierro
+        para una linea, divide el hierro quetienes entre 30 y te da el numero
+        esacto de lineas de producion que puedes hacer.
+        """
+        
+        if iron_sheets>=1:
+            print("You can produce",iron_sheets,"lines of iron sheets")
+            smelt_and_construct(iron_ppm,iron_sheets)
+            conveyors(iron_sheets)
     
-else:
-    print("You cannot produce iron sheets.")
+        else:
+            print("You cannot produce iron sheets.")
     
-if iron_rods>=1:
-    print("You can produce",iron_rods,"lines of iron rods")
-    smelt_and_construct(iron_ppm,iron_rods)
-    conveyors(iron_rods)
+        if iron_rods>=1:
+            print("You can produce",iron_rods,"lines of iron rods")
+            smelt_and_construct(iron_ppm,iron_rods)
+            conveyors(iron_rods)
 
-else:
-    print("You cannot produce iron rods.")
+        else:
+            print("You cannot produce iron rods.")
     
-if screws>=1:
-    print("You can produce",screws,"lines of screws")
-    smelt_and_construct(iron_ppm,screws)
-    print("You also need", iron_rods,\
-          "more constructors for metal pipes for the screws.")
-    conveyors(screws)
+        if screws>=1:
+            print("You can produce",screws,"lines of screws")
+            smelt_and_construct(iron_ppm,screws)
+            print("You also need", iron_rods,\
+            "more constructors for metal pipes for the screws.")
+            conveyors(screws)
 
-else:
-    print("You cannot produce screws.")
+        else:
+            print("You cannot produce screws.")
 """
 Estos "if" y "else" te dicen si puedes producir una linea de un material o si no
 tienes suficiente material.
 """
+print("Thank you for using the Ficsit Efficient Factory Module")
